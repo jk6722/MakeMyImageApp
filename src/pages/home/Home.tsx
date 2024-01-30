063425e9d2dbbde48c78a0ff499bf261e270158d
재국 <jk6722@hanyang.ac.kr>
2024-01-30 15:43:52 +0900

    style: 로딩 컴포넌트 적용

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 import EmptyImage from "./components/EmptyImage";
 import InputKeyword from "./components/InputKeyword";
+import Loading from "../../components/Loading";
 
 const Home = () => {
   const [imageUrl, setImageUrl] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
     <div className="w-full h-full flex flex-col items-center">
-      <div className="w-[70%] bg-white rounded-b-xl rounded-tr-xl flex flex-col items-center justify-center py-[30px]">
+      <div className="w-[70%] relative bg-white rounded-b-xl rounded-tr-xl flex flex-col items-center justify-center py-[30px]">
         <div className="text-[24px]">Image Generator</div>
+        {isLoading && <Loading />}
         {imageUrl ? (
-          <div className="w-full flex items-center justify-center py-7">
+          <div className="w-[470px] flex items-center justify-center py-7">
             <img src={imageUrl} style={{ borderRadius: "6px" }} />
           </div>
         ) : (
           <EmptyImage />
         )}
-        <InputKeyword setImageUrl={setImageUrl} />
+        <InputKeyword setImageUrl={setImageUrl} setIsLoading={setIsLoading} />
       </div>
     </div>
   );
